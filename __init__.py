c2a4f21985c490191d41a756e6222b2bab833f7f
jarrod.millman <jarrod.millman@d6536bca-fef9-0310-8506-e4c0a848fbcf>
2007-08-29 18:24:04 +0000

    moved scipy/sandbox/models to scipy/stats/models
    git-svn-id: http://svn.scipy.org/svn/scipy/trunk/scipy/stats/models@3283 d6536bca-fef9-0310-8506-e4c0a848fbcf

--- __init__.py
+++ __init__.py
@@ -4,15 +4,15 @@
 
 __docformat__ = 'restructuredtext'
 
-from scipy.sandbox.models.info import __doc__
+from scipy.stats.models.info import __doc__
 
-import scipy.sandbox.models.model
-import scipy.sandbox.models.formula
-import scipy.sandbox.models.regression
-import scipy.sandbox.models.robust
-import scipy.sandbox.models.family
-from scipy.sandbox.models.glm import model as glm
-from scipy.sandbox.models.rlm import model as rlm
+import scipy.stats.models.model
+import scipy.stats.models.formula
+import scipy.stats.models.regression
+import scipy.stats.models.robust
+import scipy.stats.models.family
+from scipy.stats.models.glm import model as glm
+from scipy.stats.models.rlm import model as rlm
 
 __all__ = filter(lambda s:not s.startswith('_'),dir())
 
--- robust/__init__.py
+++ robust/__init__.py
@@ -4,5 +4,5 @@ Robust statistical models
 import numpy as N
 import numpy.linalg as L
 
-from scipy.sandbox.models.robust import norms
-from scipy.sandbox.models.robust.scale import MAD
+from scipy.stats.models.robust import norms
+from scipy.stats.models.robust.scale import MAD
